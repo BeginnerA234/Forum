3c0e9521d335a50cdbc9050c5cfc9f3ae5dc1bff
Kirill <kirilenkoboss@gmail.com>
2021-08-13 00:59:05 +0300

    Жизнь токенов

--- backend/settings.py
+++ backend/settings.py
@@ -156,8 +156,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(seconds=20),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=6),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=4),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
