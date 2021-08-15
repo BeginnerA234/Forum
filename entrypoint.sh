d733ef46ee41aae8f20e8741d4da31213df52d67
Kirill <kirilenkoboss@gmail.com>
2021-08-15 05:11:06 +0300

    Docker-compose: nginx, gunicorn, postgres

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,4 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py loaddata custom_user_app.json
 python manage.py loaddata forum_app.json
+exec gunicorn backend.wsgi:application -b 0.0.0.0:8000 -w 4 --reload
